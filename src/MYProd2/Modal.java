21bf77752f141fbcb903e85d0f3f206f330cb499
RKUMA475 <RKUMA475@LIN41006710.corp.capgemini.com>
2022-11-21 10:39:27 +0530

    New Modal Class Added

---
+++ src/MYProd2/Modal.java
@@ -0,0 +1,21 @@
+package MYProd2;
+
+public class Modal {
+	
+	private String Name;
+	private int age;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
